e0ceb3b134791faa0221e4ffa6e54b1cbfc0b2f4
const-ae <artjom31415@googlemail.com>
2020-06-22 13:58:09 +0200

    Write documentation

---
+++ R/einsum-package.R
@@ -0,0 +1,13 @@
+
+#' Einstein Summation
+#'
+#' Einstein summation is a concise mathematical notation that
+#' implicitly sums over repeated indices of n-dimensional arrays. Many ordinary
+#' matrix operations (e.g. transpose, matrix multiplication, scalar product, 'diag()', trace etc.)
+#' can be written using Einstein notation. The notation is particularly convenient for
+#' expressing operations on arrays with more than two dimensions because the
+#' respective operators ('tensor products') might not have a standardized name.
+#'
+#' @docType  package
+#' @name einsum_package
+NULL
